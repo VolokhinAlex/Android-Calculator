99d551481a423554f6e561f59af4dd5eb5852483
Aleksey Volokhin <kanopus05@mail.ru>
2022-10-14 18:21:14 +0700

    1. Создать проект со следующими пользовательскими элементами: TextView, EditText, Button, Switch, CheckBox, ToggleButton. Для работы использовать LinearLayout. Использовать различные шрифты, цвета, размеры, прочие атрибуты. 2. Создать ещё один макет (если не получается, то проект) с несколькими EditText, где использовать атрибут inputType: text, textPersonName, phone, number, textPassword, textEmailAddress и другие значения. 3. Добавить в проект элемент CalendarView.

--- app/src/main/java/com/example/java/android1/javaandroid1calculator/MainActivity.java
+++ app/src/main/java/com/example/java/android1/javaandroid1calculator/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.java.android1.javaandroid1calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.form_layout);
+
+        CalendarView calendarView = findViewById(R.id.calendar_view);
+        calendarView.setVisibility(View.INVISIBLE);
+        EditText dateTime = findViewById(R.id.date_field);
+
+        dateTime.setOnClickListener(view -> {
+            calendarView.setVisibility(View.VISIBLE);
+            calendarView.setOnDateChangeListener((calendarView1, year, month, dayOfMonth) -> {
+                dateTime.setText(String.format("%s/%s/%s", month, dayOfMonth, year));
+                calendarView.setVisibility(View.INVISIBLE);
+            });
+        });
     }
 }
