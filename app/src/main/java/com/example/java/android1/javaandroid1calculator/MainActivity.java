d1b02dcd09f88b12430078b64aa36f40222ee167
Aleksey Volokhin <kanopus05@mail.ru>
2022-10-15 15:04:43 +0700

    Добавлен переход по окнам. Добавлен вывод имени в MainActivity по нажатию кнопки.

--- app/src/main/java/com/example/java/android1/javaandroid1calculator/MainActivity.java
+++ app/src/main/java/com/example/java/android1/javaandroid1calculator/MainActivity.java
@@ -2,27 +2,40 @@ package com.example.java.android1.javaandroid1calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.CalendarView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.form_layout);
-        CalendarView calendarView = findViewById(R.id.calendar_view);
-        calendarView.setVisibility(View.INVISIBLE);
-        EditText dateTime = findViewById(R.id.date_field);
+        setContentView(R.layout.activity_main);
+        onShowFirstName();
+        switchActivity();
+    }
 
-        dateTime.setOnClickListener(view -> {
-            calendarView.setVisibility(View.VISIBLE);
-            calendarView.setOnDateChangeListener((calendarView1, year, month, dayOfMonth) -> {
-                dateTime.setText(String.format("%s/%s/%s", month, dayOfMonth, year));
-                calendarView.setVisibility(View.INVISIBLE);
-            });
+    private void switchActivity() {
+        Button formActivity = findViewById(R.id.form_activity);
+        formActivity.setOnClickListener(click -> {
+            Intent intent = new Intent(this, FormActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void onShowFirstName() {
+        Button showName = findViewById(R.id.show_name);
+        EditText inputFirstName = findViewById(R.id.name);
+        TextView firstName = findViewById(R.id.first_name);
+        showName.setOnClickListener(click -> {
+            if (!String.valueOf(inputFirstName.getText()).trim().equals("")) {
+                firstName.setText(inputFirstName.getText());
+            } else {
+                firstName.setText("Please, enter your name");
+            }
         });
     }
 }
