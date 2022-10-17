c929f77c8da045bd4ea24a4a9e1edf80f2cbb778
alexvolokhin <arsarsi644@gmail.com>
2022-10-17 16:35:21 +0200

    Добавил метод для работы с элементами checkBox и Switch.

--- app/src/main/java/com/example/java/android1/javaandroid1calculator/MainActivity.java
+++ app/src/main/java/com/example/java/android1/javaandroid1calculator/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         onShowFirstName();
         switchActivity();
+        actionsWithCheckBoxAndSwitch();
     }
 
     private void switchActivity() {
@@ -28,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void onShowFirstName() {
         Button showName = findViewById(R.id.show_name);
-        EditText inputFirstName = findViewById(R.id.name);
+        EditText inputFirstName = findViewById(R.id.input_first_name);
         TextView firstName = findViewById(R.id.first_name);
         showName.setOnClickListener(click -> {
             if (!String.valueOf(inputFirstName.getText()).trim().equals("")) {
@@ -38,4 +43,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void actionsWithCheckBoxAndSwitch() {
+        CheckBox checkBox = findViewById(R.id.checkbox);
+        Switch showOrCloseText = findViewById(R.id.switch_turn);
+        TextView textView = findViewById(R.id.hello_android_text);
+        checkBox.setOnClickListener(event -> {
+            if (checkBox.isChecked()) {
+                checkBox.setText("Turn off");
+            } else {
+                checkBox.setText("Turn on");
+            }
+        });
+        showOrCloseText.setOnClickListener(event -> {
+            if (showOrCloseText.isChecked()) {
+                showOrCloseText.setText("Close Text");
+                textView.setVisibility(View.VISIBLE);
+            } else {
+                showOrCloseText.setText("Show Text");
+                textView.setVisibility(View.GONE);
+            }
+        });
+    }
+
 }
